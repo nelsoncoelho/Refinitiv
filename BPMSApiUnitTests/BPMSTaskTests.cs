c6e36a13fa491db40afe2f2c34cee4afb64b93e1
Nelson Coelho <Nelson.Coelho@timico.co.uk>
2019-08-01 22:53:29 +0100

    Created web application and added tests project.

---
+++ BPMSApiUnitTests/BPMSTaskTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BPMSApiUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetBPMSTasksTest()
+        {
+        }
+
+        [TestMethod]
+        public void PutBPMSTasksTest()
+        {
+        }
+    }
+}
---
+++ BPMSApiUnitTests/TestBase.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using BPMSApi;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMSApiUnitTests
+{
+    public abstract class TestBase
+    {
+        public BPMSApiInstance Instance { get; set; }
+
+
+        // Ideally these would have they're own project to escalate the number of secrets per API.
+        #region Secrets definition
+        private static AmazonSecretsManagerClient Client = new AmazonSecretsManagerClient(RegionEndpoint.EUWest2);
+        private static async Task<T> GetSecrets<T>(String secretName)
+        {
+            var secret = await Client.GetSecretValueAsync(new GetSecretValueRequest
+            {
+                SecretId = secretName
+            });
+            return JsonConvert.DeserializeObject<T>(secret.SecretString);
+        }
+
+        public static async Task<BPMSConfig> GetBPMSSecrets() => await GetSecrets<BPMSConfig>("BPMSCredentials");
+
+        #endregion
+
+
+
+
+        protected TestBase()
+        {
+            var credentials = GetBPMSSecrets().Result;
+
+            Instance = new BPMSApiInstance(new BPMSConfig
+            {
+                // Here you can pass the configurations for authentication of the API call.
+                // ApiKey = credentials.ApiKey,
+                // Credentials = credentials.Credentials
+            });
+        }
+
+    }
+}
